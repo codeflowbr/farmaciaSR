46b111311ff48fd581782134febaa964bd3ceadb
patrick <patrickissler1234@gmail.com>
2023-08-08 13:48:07 -0300

    login

--- src/main/java/com/codeflow/api/LoginController.java
+++ src/main/java/com/codeflow/api/LoginController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codeflow.dto.LoginDTO;
-import com.codeflow.service.CustomerService;
 import com.codeflow.service.UsuarioService;
 
 @RestController
