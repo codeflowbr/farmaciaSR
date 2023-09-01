9ca5270b5131f28c25741437207c7f9703d0ed9e
patrick <patrickissler1234@gmail.com>
2023-09-01 17:04:44 -0300

    formatar valor

--- src/main/java/com/codeflow/service/impl/VencimentoServiceImpl.java
+++ src/main/java/com/codeflow/service/impl/VencimentoServiceImpl.java
@@ -26,7 +26,7 @@ public class VencimentoServiceImpl implements VencimentoService {
 			String nomeProduto = venda.getProdutos().get(0).getNome();
 			String nomeCliente = venda.getCliente().getNome();
 			String mensagem;
-			if(venda.getProdutos().get(0).getDesconto() != 0) {			
+			if(venda.getProdutos().get(0).getDesconto() != 0 && venda.getProdutos().get(0).getValor() != venda.getProdutos().get(0).getDesconto() ) {			
 				float valorDesconto = (venda.getProdutos().get(0).getDesconto());
 				mensagem = "Olá "+nomeCliente+", tudo bem? Somos da Farmácia São Rafael e notamos que o seu medicamento, "+nomeProduto+", está terminando e tomamos a liberdade de separá-lo para você. Assim, poderá estar retirando em nossa loja ou se preferir, entregamos onde você estiver. O valor do produto é de "+venda.getProdutos().get(0).getValor()+", mas com o seu desconto, ele fica por "+valorDesconto+". Gostaria de antecipar sua compra para não faltar sua medicação?";
 			}else {
